553e6632d3289a3649d68576009089edfca1e9da
JaverJaver <mykhailo.kornukh@gmail.com>
2024-11-12 11:25:42 +0200

    init

---
+++ src/test/java/com/spring/booking/accommodationbookingservice/AccommodationBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.booking.accommodationbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
